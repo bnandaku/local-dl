58d30298613930e02d04bbadcc12acfdd0461e0f
bharat <br@endspam.io>
2023-06-19 01:42:44 -0400

    fixing a quick bug

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -113,6 +114,36 @@ func (i *Item) StartDownload() error {
 
 	return nil
 
+}
+
+func update(name string) {
+	url := "https://putio.bramsoft.com/update"
+	method := "POST"
+
+	payload := strings.NewReader(fmt.Sprintf("{\"name\": \"%s\"}", name))
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
 }
 
 func HandleDownload(c *gin.Context) {
